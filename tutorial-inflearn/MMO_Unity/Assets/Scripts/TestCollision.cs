8a13b6a9200f7ffaaed8284c55eb643bc79f191a
casualprogramming <syc9624@gmail.com>
2021-01-06 18:50:30 +0900

    create raycast example

--- tutorial-inflearn/MMO_Unity/Assets/Scripts/TestCollision.cs
+++ tutorial-inflearn/MMO_Unity/Assets/Scripts/TestCollision.cs
@@ -6,11 +6,15 @@ public class TestCollision : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision !");
+        //Debug.Log($"Collision Enter@{collision.gameObject.name}!");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        //Debug.Log($"Collision Exit@{collision.gameObject.name}!");
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger !");
+        //Debug.Log("Trigger !");
     }
     void Start()
     {
@@ -19,6 +23,19 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
-        
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+        RaycastHit hit;
+
+        RaycastHit[] hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+        foreach (var h in hits)
+        {
+            Debug.Log($"Raycast all !{h.collider.name}");
+        }
+        if (Physics.Raycast(transform.position + Vector3.up, look, out hit, 10))
+        {
+            
+            //Debug.Log($"Raycast !{hit.collider.gameObject.name}");
+        }
     }
 }
