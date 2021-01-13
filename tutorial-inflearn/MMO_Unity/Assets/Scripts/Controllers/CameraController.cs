49f3dc2792ac45025c78e0f6df6a960fcb42271f
casualprogramming <syc9624@gmail.com>
2021-01-13 19:44:19 +0900

    create CameraController (quater view camera)
    quaterview camera update

--- tutorial-inflearn/MMO_Unity/Assets/Scripts/Controllers/CameraController.cs
+++ tutorial-inflearn/MMO_Unity/Assets/Scripts/Controllers/CameraController.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField]
+    Define.CameraMode _mode = Define.CameraMode.QuaterView;
+    [SerializeField]
+    Vector3 _deltas = new Vector3(0.0f, 6.0f, -5.0f);
+    [SerializeField]
+    GameObject _player = null;
     void Start()
     {
         
     }
 
-    void Update()
+    void LateUpdate()//Ensure that this function runs after player controller update
     {
-        
+        if(_mode == Define.CameraMode.QuaterView)
+        {
+            transform.position = _player.transform.position + _deltas;
+            transform.LookAt(_player.transform.position);
+        }
+    }
+
+    public void SetQuaterView(Vector3 delta)
+    {
+        _mode = Define.CameraMode.QuaterView;
+        _deltas = delta;
     }
 }
