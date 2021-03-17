ea90cc8260f383a322eaf68f55d39de981516079
casualprogramming <syc9624@gmail.com>
2021-03-18 00:01:47 +0900

    add bindfunc of gameobject to UI_button automation

--- tutorial-inflearn/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
+++ tutorial-inflearn/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
@@ -4,11 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
     [SerializeField]
     Text _text;
-    Dictionary<Type,UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();//for reflection
 
     //in this case, enum is used for unique id not for type
     enum Buttons
@@ -21,35 +20,20 @@ public class UI_Button : MonoBehaviour
         ScoreText,
         PointText
     }
+    enum GameObjects
+    {
+        TestObject,
+    }
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
 
         Get<Text>((int)Texts.PointText).text="Bind Test";
     }
 
-    void Bind<T>(Type type) where T:UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
-
-        for(int i=0;i<names.Length;i++)
-        {
-            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-        }
-    }
-
-    T Get<T>(int idx) where T : UnityEngine.Object
-    {
-        UnityEngine.Object[] objects = null;
-        if(_objects.TryGetValue(typeof(T),out objects)==false)
-            return null;
-        return objects[idx] as T;
-    }
-
-    int _score=0;
+    int _score =0;
     public void OnButtonClicked()
     {
         _score++;
