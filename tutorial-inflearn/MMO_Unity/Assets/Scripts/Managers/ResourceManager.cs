8a400d1a2f3da72b662aeaffdd446823b068594c
casualprogramming <syc9624@gmail.com>
2021-04-20 14:30:00 +0900

    bug fix

--- tutorial-inflearn/MMO_Unity/Assets/Scripts/Managers/ResourceManager.cs
+++ tutorial-inflearn/MMO_Unity/Assets/Scripts/Managers/ResourceManager.cs
@@ -23,8 +23,7 @@ public class ResourceManager
         int index = go.name.IndexOf("(Clone)");
         if(index > 0)
             go.name = go.name.Substring(0,index);
-
-        return Object.Instantiate(prefab, parent);
+        return go;
     }
 
     public void Destory(GameObject go)
