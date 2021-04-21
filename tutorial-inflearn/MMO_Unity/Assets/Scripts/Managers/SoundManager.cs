05fbe7d71c4a983193287c951bb25a4026cb7d47
casualprogramming <syc9624@gmail.com>
2021-04-21 14:49:57 +0900

    [Sound Manager] test sound system

---
+++ tutorial-inflearn/MMO_Unity/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager
+{
+    //MP3 Player -> AudioSource
+    //MP3 Music -> AudioClip
+    //audience -> AudioListener
+}
